45b0149483c8d912aa26122672bb2ff7747c1d66
AiAl5 <alihalabi262@gmail.com>
2024-11-10 16:08:06 -0700

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+function showSection(sectionId) {
+    const sections = document.querySelectorAll(".section");
+  
+    sections.forEach((section) => {
+      section.classList.remove("active");
+      if (section.id === sectionId) {
+        section.classList.add("active");
+      }
+    });
+  }
+  
